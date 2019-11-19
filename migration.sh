b5691a94651c5bcdedd27dd6a73d084eb114412d
Brien Hartung <brien.hartung@outlook.com>
2019-11-19 20:02:53 +0000

    Update migration.sh

--- migration.sh
+++ migration.sh
@@ -4,10 +4,14 @@
 
 USERLIST="floodand minnesot" 
 
+LOG=migration.log
+exec 1>$LOG 2>&1
+
 for USER in $USERLIST
 do
   /scripts/pkgacct $USER
-  scp -p 2232 cpmove-$USER.tar.gz brien@45.79.6.53:/mnt/http_data/bkps_httpd2/
+  FILENAME="cpmove-$USER.tar.gz"
+  scp -P 2232 $FILENAME brien@45.79.6.53:/mnt/http_data/bkps_httpd2/
   CMD="/scripts/restorepkg $user"
   ssh brien@45.79.6.53 -p 2232 "$CMD"
   echo "$USER migrated"
