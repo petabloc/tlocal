8dbc35d85b87c7122b95771f675bfb0bc5782a40
Brien Hartung <brien.hartung@outlook.com>
2019-11-19 19:43:24 +0000

    Update scp-test.sh
    It works!

--- scp-test.sh
+++ scp-test.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 #testing scp and ssh over shell scripts
- 
+
 # This should result in three text files in home directory, copies of same
 # in home directory of other server, and a text file with three lines in it 
 # in other server.
- 
-USERLIST="alice bob charlie"
 
+USERLIST="alice bob charlie"
+ 
 LOG=scp-test.log
 exec 1>$LOG 2>&1
 for USER in $USERLIST
@@ -15,7 +15,7 @@ do
   echo "attempting $USER"
   FILENAME="$USER.txt"
   echo "This is a test for $USER" > $FILENAME
-  scp -p 2232 $FILENAME brien@45.79.6.53:/tlm-admins/brien/
+  scp -P 2232 $FILENAME brien@45.79.6.53:/tlm-admins/brien/
   CMD="echo 'happy little text' >> /tlm-admins/brien/result.txt"
   ssh brien@45.79.6.53 -p 2232 "$CMD"
 done
