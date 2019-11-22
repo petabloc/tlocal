29b3dc02a0799801b2cb643e8ed40858dad3f686
Brien Hartung <brien.hartung@outlook.com>
2019-11-22 14:27:02 +0000

    Update migration-2nd-phase.sh
    Command is working.
    
    Still had to add a (unsightly and inefficient) move to /web, because specifying a path to the restorepkg script did not seem to work. 
    
    This page says you can just specify a path to the archive, but it didn't work.
    https://forums.cpanel.net/threads/fully-explain-scripts-restorepkg.250472/

--- migration-2nd-phase.sh
+++ migration-2nd-phase.sh
@@ -4,20 +4,13 @@ source includes
 # For 2nd phase of automated migration of wordpress sites for targetlocal job
 
 
-LOG=migration.log
+LOG=migration-2nd-phase.log
 exec 1>$LOG 2>&1
 
-
-for USER in `ls $DEST_DIR/*.tar.gz|sed 's/cpmove-//'|cut -f1 -d'.'`
+sudo mv /mnt/httpd2_backups/imports/* /web/
+for USER in `ls $DEST_DIR|sed 's/cpmove-//'|cut -f1 -d'.'`
 
 do
-
-#  mkfs.ext4 /dev/sdsomething
-#  mount /dev/sdsomething /imports
-
-# Work with viket to create a temporary disk for imports with enough free disk
-# sudo cp cpmove-$USER.tar.gz /imports
-## /mnt/http_data/bkps_httpd2/ is not working
   /scripts/restorepkg $USER
+  echo "$USER restored"
 done
-
